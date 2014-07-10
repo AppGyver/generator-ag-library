fa57cb2b70487e04d20bfa2d9497af128d54ca19
Eevert Saukkokoski <eevert.saukkokoski@appgyver.com>
2014-07-10 15:02:31 +0300

    rename to generator-ag-library

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,14 +5,14 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
 
-describe('ag generator', function () {
+describe('ag-library generator', function () {
     beforeEach(function (done) {
         helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
             if (err) {
                 return done(err);
             }
 
-            this.app = helpers.createGenerator('ag:app', [
+            this.app = helpers.createGenerator('ag-library:app', [
                 '../../app'
             ]);
             done();
